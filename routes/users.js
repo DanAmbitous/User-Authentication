e48f059193e18368d7c0cb3ef8175aa5975d236e
Danial A <danazad2016@gmail.com>
2021-06-04 23:57:43 -0500

    Added login functionality via the username and password

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,18 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/userPrototype')
 
+let usersArray;
+
+async function arrayifiyData() {
+  usersArray = await User.find();
+}
+
+arrayifiyData()
+
 //Get all users
 router.get('/', async (req, res) => {
   try {
-    const users = await User.find()
+   const users = await User.find()
 
     res.json(users)
   } catch (error) {
@@ -15,15 +23,13 @@ router.get('/', async (req, res) => {
 })
 
 //Get one user
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getAUser, (req, res) => {
+  res.send(res.user)
 })
 
 //Create a user
 router.post('/', async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-  console.log(hashedPassword)
 
   const user = new User({
     username: req.body.username,
@@ -40,17 +46,21 @@ router.post('/', async (req, res) => {
 
 //Login with POST method
 router.post('/login', async (req, res) => {
-  console.log(User)
+  const user = usersArray.find(user => user.username === req.body.username)
 
-  const user = User.find(user => user.username === req.body.username)
+  console.log(user)
+
+  console.log(req.body.username)
 
   if (user == null) {
     return res.status(400).send('Cannot find user')
   }
 
   try {
-    if (await bcrypt.compare(req.body.password, User.password)) {
-      res.send('Sucess')
+    console.log(req.body.password, user.password)
+
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
     } else {
       res.send('Not Allowed')
     }
